d6579fe4c8ef895513e602f0f5482225f4f595d4
SHarsheev <51026122+sharsheevnursultan@users.noreply.github.com>
2019-06-28 18:35:40 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        touchDrag:true,
+        navElement:true,
+        dots:true,
+        nav:true,
+        navText:false,
+        // autoplay: true,
+        // autoplayTimeout: 4000,
+    });
